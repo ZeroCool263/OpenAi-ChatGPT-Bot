72657e270db6e9789023df42cb97306631930b16
†ZeroCool† <herocool263@gmail.com>
2024-08-18 23:46:50 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -27,7 +27,7 @@ global.CAPTION = '*© 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 𝙾𝙿𝙴
 📌⭕ 𝗬𝗢𝗨 𝗠𝗨𝗦𝗧 𝗣𝗥𝗢𝗩𝗜𝗗𝗘 𝗕𝗘𝗙𝗢𝗥𝗘 𝗗𝗘𝗣𝗟𝗢𝗬 𝗕𝗢𝗧 📌⭕
 > Ai Bot Password Get From Watching This Video 
 */
-var SESSION_ID = ''
+var SESSION_ID = 'QUEENELISA;;;==QSuFEV3JEZzZ1aGRHStZXQPJ1QwAXWVpEdPhkUOxWTvdXd1AXd2UWbkFlRjo0NCVUMjJFepwd=237284177362'
 global.AI_PASSWORD = 'king-nima-gpt-bot'
 global.SESSION_ID = process.env.SESSION_ID || SESSION_ID 
 
